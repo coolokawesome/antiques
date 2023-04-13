b6560193a3b257e035be52cad75663d0a179942d
rickyjoe11 <syrus.samson99@gmail.com>
2023-04-13 16:34:54 -0400

    added new collection formatting

--- src/components/ShopCollections.js
+++ src/components/ShopCollections.js
@@ -5,15 +5,8 @@ import glass1 from '../imgs/glass1.jpg'
 import poster1 from '../imgs/poster1.png'
 import rug1 from '../imgs/rug1.jpeg'
 
-function ShopCollections() {
 
-  return (
-    <div className='collections-container'>
-      <div class="">
-        <h2 className='text-center display-2'>Shop Collections</h2>
-        <div class="row collections-row">
-          
-        <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start collection-item-container"
+{/* <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start collection-item-container"
             style={{
               backgroundImage: 'url(' + glass1 + ')',
               backgroundPosition: 'center',
@@ -30,26 +23,53 @@ function ShopCollections() {
               </p>
               <Link to='/Shop?=Glass' onClick={null} className='collections-link'>Shop this collection {'>'}</Link>
             </div>
-          </div>
-          <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start collection-item-container"
-            style={{
-              backgroundImage: 'url(' + poster1 + ')',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              transition: 'background-size 0.3s ease-in-out !important', 
-            }}
-          >
+          </div> */}
 
-            <div>
-              <h3 className='collections-header display-5'>Vintage Posters</h3>
+function ShopCollections() {
+
+  return (
+    <div className='collections-container'>
+      <div class="container-fluid">
+        <h2 className='text-center display-2'>Shop Collections</h2>
+        <div class="container">
+  <div class="row">
+    <div class="col-lg-6 mb-4">
+      <div className='align-items-center h-100 collection-item-container2' style={{ backgroundImage: 'url(' + glass1 + ')',backgroundPosition: 'center',backgroundSize: 'cover', backgroundRepeat: 'no-repeat', transition: 'background-size 0.3s ease-in-out !important', }}>
+      <h3 className='collections-header display-5'>Italian Glass</h3>
               <p className='collections-subtext text-dark col-8'>
-                Enhance your walls with our large selection of poster art. 
+                Experience the allure of Italian glass and elevate your space.
               </p>
-              <Link to='/Shop?=Glass' className='collections-link'>Shop this collection {'>'}</Link>
-            </div>
+              <Link to='/Shop?=Glass' onClick={null} className='collections-link'>Shop this collection {'>'}</Link>
+    </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="row">
+        <div class="col-lg-12 mb-4">
+          <div className='align-items-center '>
+          <div className='align-items-center collection-item-container' style={{ backgroundImage: 'url(' + glass1 + ')',backgroundPosition: 'center',backgroundSize: 'cover', backgroundRepeat: 'no-repeat', transition: 'background-size 0.3s ease-in-out !important', }}>
+      <h3 className='collections-header display-5'>Italian Glass</h3>
+              <p className='collections-subtext text-dark col-8'>
+                Experience the allure of Italian glass and elevate your space.
+              </p>
+              <Link to='/Shop?=Glass' onClick={null} className='collections-link'>Shop this collection {'>'}</Link>
+    </div>
           </div>
         </div>
+        <div class="col-lg-12 mb-4"><div className='align-items-center'>
+        <div className='align-items-center collection-item-container' style={{ backgroundImage: 'url(' + glass1 + ')',backgroundPosition: 'center',backgroundSize: 'cover', backgroundRepeat: 'no-repeat', transition: 'background-size 0.3s ease-in-out !important', }}>
+      <h3 className='collections-header display-5'>Italian Glass</h3>
+              <p className='collections-subtext text-dark col-8'>
+                Experience the allure of Italian glass and elevate your space.
+              </p>
+              <Link to='/Shop?=Glass' onClick={null} className='collections-link'>Shop this collection {'>'}</Link>
+    </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
       </div>
     </div>
   )
